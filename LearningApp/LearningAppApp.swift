498dc11fb35ea2b94594475eef7208f0046fc42d
adiln2002 <adil.almarzooq@gmail.com>
2022-10-10 12:29:18 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by ADEL ALMARZOOQ on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
