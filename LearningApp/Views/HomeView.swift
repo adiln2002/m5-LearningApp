8049fa1bf25f07570cde1573ce628f301744ad46
adiln2002 <adil.almarzooq@gmail.com>
2022-10-12 08:24:30 +0300

    Add home view add subview to organize the code

---
+++ LearningApp/Views/HomeView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by ADEL ALMARZOOQ on 10/10/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack (alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    
+                    LazyVStack {
+                        
+                        ForEach(model.modules) { module in
+                            
+                            VStack (spacing: 20) {
+                                // Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                            }
+                        }
+                        
+                    }
+                    .padding()
+                    
+                }
+            }
+            .navigationTitle("Get Started")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .environmentObject(ContentModel())
+    }
+}
